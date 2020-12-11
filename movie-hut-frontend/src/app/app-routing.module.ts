52ff569f0ee90fd265da0c4bff6383f1319ceaf8
SiddAjriY2Jaccount <y2jsiddajri@gmail.com>
2020-12-12 02:44:40 +0530

    new frontend updated

--- movie-hut-frontend/src/app/app-routing.module.ts
+++ movie-hut-frontend/src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { BookingComponent } from './booking/booking.component';
-import { MoviesComponent } from './movies/movies.component';
+import { HomeComponent } from './components/home/home.component';
+import { BookingComponent } from './components/booking/booking.component';
+import { MoviesComponent } from './components/movies/movies.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
--- movie-hut-frontend/src/app/app.module.ts
+++ movie-hut-frontend/src/app/app.module.ts
@@ -4,10 +4,10 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CustomNavbarComponent } from './custom-navbar/custom-navbar.component';
-import { BookingComponent } from './booking/booking.component';
-import { MoviesComponent } from './movies/movies.component';
-import { HomeComponent } from './home/home.component';
+import { CustomNavbarComponent } from './components/custom-navbar/custom-navbar.component';
+import { BookingComponent } from './components/booking/booking.component';
+import { MoviesComponent } from './components/movies/movies.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
